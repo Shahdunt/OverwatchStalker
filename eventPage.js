c9250877ac9196c9846def4426d3b0de9d366f44
Shahdunt <38637991+Shahdunt@users.noreply.github.com>
2018-04-23 00:22:03 -0300

    Name change, Playoverwatch options
    -Name change to "Overwatch Stalker"
    -Added Playoverwatch search options

--- eventPage.js
+++ eventPage.js
@@ -10,8 +10,22 @@ var menuItemOverbuffSearch = {
     "contexts": ["selection"]
 };
 
+var menuItemBlizzDirect = {
+    "id": "BlizzDirect",
+    "title": "PlayOverwatch",
+    "contexts": ["selection"]
+};
+
+var menuItemBlizzSearch = {
+    "id": "BlizzSearch",
+    "title": "PlayOverwatch Search",
+    "contexts": ["selection"]
+};
+
 chrome.contextMenus.create(menuItemOverbuffDirect);
 chrome.contextMenus.create(menuItemOverbuffSearch);
+chrome.contextMenus.create(menuItemBlizzDirect);
+chrome.contextMenus.create(menuItemBlizzSearch);
 
 chrome.contextMenus.onClicked.addListener(function(selection){   
     if(selection.selectionText)
@@ -21,13 +35,20 @@ chrome.contextMenus.onClicked.addListener(function(selection){
         switch(selection.menuItemId)
         {
             case "OverbuffSearch":
-            urlBase = "https://www.overbuff.com/search?q=";
-            strUrl = urlBase + selection.selectionText.replace("#", "%23");
+                urlBase = "https://www.overbuff.com/search?q=";
+                strUrl = generateBattleTag(urlBase, selection.selectionText, "%23");
             break;
-
             case "OverbuffDirect":
-            urlBase = "https://www.overbuff.com/players/pc/";
-            strUrl = urlBase + selection.selectionText.replace("#", "-");
+                urlBase = "https://www.overbuff.com/players/pc/";
+                strUrl = generateBattleTag(urlBase, selection.selectionText, "-");
+            break;
+            case "BlizzDirect":
+                urlBase = "https://playoverwatch.com/en-us/career/pc/";
+                strUrl = generateBattleTag(urlBase, selection.selectionText, "-");
+            break;
+            case "BlizzSearch":
+                urlBase = "https://playoverwatch.com/en-us/search?q=";
+                strUrl = generateBattleTag(urlBase, selection.selectionText, "-");
             break;
         }
         openInNewTab(strUrl);
@@ -37,4 +58,10 @@ chrome.contextMenus.onClicked.addListener(function(selection){
 function openInNewTab(url) {
     var win = window.open(url, '_blank');
     win.focus();
+}
+
+function generateBattleTag(urlBase, battleTag, strReplace){
+    var strUrl;
+    strUrl = urlBase + battleTag.replace("#", strReplace);
+    return strUrl;
 }
